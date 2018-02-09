80c5929d832ac1f5bf5067be24db1fd2947e6496
Steffen Küllmer <30567862+nullsibnelf@users.noreply.github.com>
2018-02-09 16:45:16 +0100

    .

--- software/arduino/smart_whoop_gate/smart_whoop_gate.ino
+++ software/arduino/smart_whoop_gate/smart_whoop_gate.ino
@@ -7,12 +7,12 @@
 #define NeoPixelVCC    2
 #define NeoPixelPin    3
 #define NeoPixelAmount 8
-#define Taster         9
+#define Pushbutton     9
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NeoPixelAmount, NeoPixelPin, NEO_GRB + NEO_KHZ800);
-RF24 radio(7, 8);                   // CE, CSN
+RF24 radio(7, 8);                                                 // CE, CSN of wifi module
 
-const byte address[6] = "00001";
+const byte address[6] = "00001";                                  // wifi net name
 
 int node_nr = 0;
 int my_node_nr = 0;
@@ -26,24 +26,25 @@ int radio_input = 0;
 
 void setup() 
 {
-  radio.begin();
-  radio.setPALevel(RF24_PA_MIN);
+  radio.begin();                                                  // wifi module init
+  radio.setPALevel(RF24_PA_MAX);
   radio.openReadingPipe(0, address);
   radio.startListening();
   
-  pinMode(Taster, INPUT_PULLUP);
-  
+  pinMode(Pushbutton, INPUT_PULLUP);                              // hw init
   pinMode(NeoPixelVCC, OUTPUT);
   digitalWrite(NeoPixelVCC, HIGH);
-  pinMode(NeoPixelPin, OUTPUT);  
-  pixels.begin();                             // Initialisierung der NeoPixel
+  pinMode(NeoPixelPin, OUTPUT);
+  
+  pixels.begin();                                                 // init of NeoPixel
   pixels.setPixelColor(0, 5, 5, 5);
   pixels.show();
-//Serial.begin(9600);                         // setup serial for debugging
+  
+//Serial.begin(9600);                                             // setup serial for debugging
 }
 
-// 127 -> pairing starten
-// 126 -> pairing beenden
+// 127 -> start pairing
+// 126 -> stop pairing
 
 
 
@@ -53,78 +54,80 @@ void loop()
 
   switch(level)
   {
-    case 0:                                               // Start
-              if (digitalRead(Taster) == 0)
-              { if (pull_timer < pull_timer_max)
-                  pull_timer++;  }
-              else
-              { pull_timer = 0;  }
+    case 0:                                                       // Start Menu
+              if (pull_timer == 0)
+              { for (int i=0; i<NeoPixelAmount; i++)              // background for submenu is 
+                { pixels.setPixelColor(i, 5, 5, 5);  }  }         // white
+              else                                                //
+              { show_bar(pull_timer);  }                          // debounce visualisation
 
-              if (pull_timer > 0)
-              { show_bar(pull_timer);  }
-              else
-              { for (int i=0; i<NeoPixelAmount; i++)
-                { pixels.setPixelColor(i, 5, 5, 5);  }  }
-
-              if (radio.available())
-              { radio.read(&radio_input, sizeof(radio_input));
-                if (radio_input == 127)
-                { level++;  }  }
+              if (radio.available())                              //
+              { radio.read(&radio_input, sizeof(radio_input));    //
+                if (radio_input == 127)                           // other node is master and forced config-mode
+                { level++;  }  }                                  //
               
-              if (pull_timer == pull_timer_max)
-              { while(digitalRead(Taster) == 0);
-                level++;  pull_timer = 0;  send_radio(127);  }
+              if (pull_timer == pull_timer_max)                   //
+              { while(digitalRead(Pushbutton) == 0);              // user pressed button to enter config-mode
+                level++;  pull_timer = 0;                         //
+                send_radio(127);  }                               // force other nodes to enter config-mode
               
               break;
-    case 1:                                               // Node-Config Menu
-              for (int i=0; i<NeoPixelAmount; i++)        // background for submenu in blue
-              { pixels.setPixelColor(i, 0, 0, 5);  }
+    case 1:                                                       // Node-Config Menu
+              for (int i=0; i<NeoPixelAmount; i++)                // background for submenu is
+              { pixels.setPixelColor(i, 0, 0, 5);  }              // blue
 
-              if (radio.available())
-              { radio.read(&radio_input, sizeof(radio_input));
-                node_nr = radio_input +1;  }
+              if (radio.available())                              //
+              { radio.read(&radio_input, sizeof(radio_input));    //
+                node_nr = radio_input +1;  }                      // other node enqued, increment possible own node number
               
-              if (digitalRead(Taster) == 0)
-              { send_radio(node_nr);
-                my_node_nr = node_nr;
-                while(digitalRead(Taster) == 0);
-                delay(100);
-                level++;  }
+              if (digitalRead(Pushbutton) == 0)                   //
+              { send_radio(node_nr);                              // send my own node number
+                my_node_nr = node_nr;                             //
+                while(digitalRead(Pushbutton) == 0);              // debounce
+                delay(100);                                       //
+                level++;  }                                       //
               
               break;
-    case 2:                                                 // NodeNummer anzeigen und auf finish warten bzw finish senden
-              for (int i=0; i<NeoPixelAmount; i++)
-              { pixels.setPixelColor(i, 0, 0, 0);  }              
-              pixels.setPixelColor(my_node_nr, 0, 5, 0);
+    case 2:                                                       // Node number visualisation
+              for (int i=0; i<NeoPixelAmount; i++)                // background for submenu is
+              { pixels.setPixelColor(i, 0, 0, 0);  }              // black
+              pixels.setPixelColor(my_node_nr, 0, 5, 0);          // show own node number in green
 
-              if ((my_node_nr == 0) and (digitalRead(Taster) == 0))
-              { send_radio(126);
-                while(digitalRead(Taster) == 0);
-                level++;  }
+              if ((my_node_nr == 0) and (digitalRead(Pushbutton) == 0))   //User wants to exit config mode
+              { send_radio(126);                                  // send command for exit of config-mode to other nodes
+                while(digitalRead(Pushbutton) == 0);              // debounce
+                level++;  }                                       //
               
-              if (radio.available())
-              { radio.read(&radio_input, sizeof(radio_input));
-                if (radio_input == 126)
-                { level++;  }  }   
+              if (radio.available())                              //
+              { radio.read(&radio_input, sizeof(radio_input));    //
+                if (radio_input == 126)                           // master-node (1st) forces to exit config-mode
+                { level++;  }  }                                  //
               
               break;
-    case 3:                                                 // Gates einfärben
-              for (int i=0; i<NeoPixelAmount; i++)
-              { if (my_node_nr > 0)
-                {  pixels.setPixelColor(i, 5, 0, 0);  }
-                else
-                {  pixels.setPixelColor(i, 0, 5, 0);  }  } 
-              pixels.setPixelColor(node_nr, 5, 5, 5);
-              level++;
+    case 3:                                                       // show gates in prestart condition (with gate-number in white)
+              for (int i=0; i<NeoPixelAmount; i++)                //
+              { if (my_node_nr > 0)                               // all gates are
+                {  pixels.setPixelColor(i, 5, 0, 0);  }           // red
+                else                                              //
+                {  pixels.setPixelColor(i, 0, 5, 0);  }  }        // first gate is green
+              pixels.setPixelColor(node_nr, 5, 5, 5);             // gate number is white
+              level++;                                            //
                                         
               break;
-    case 4:                                                 // rennen starten - Zeitmessung mit Überflug starten
+    case 4:                                                       // rennen starten - Zeitmessung mit Überflug starten
                           
               break;
   }
 
   pixels.show();
   delay(10);  
+  
+  if (digitalRead(Pushbutton) == 0)
+  { if (pull_timer < pull_timer_max)
+      pull_timer++;  }
+  else
+  { pull_timer = 0;  }
+
 
   // Peeper ?
   // Akku Überwachung implementieren
